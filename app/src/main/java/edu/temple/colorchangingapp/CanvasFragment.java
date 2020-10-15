7cbc0e8687affb134a3a04499b1b8c735e901461
Karl <tui98107@temple.edu>
2020-10-15 09:28:41 -0400

    create new files

---
+++ app/src/main/java/edu/temple/colorchangingapp/CanvasFragment.java
@@ -0,0 +1,7 @@
+package edu.temple.colorchangingapp;
+
+import androidx.fragment.app.Fragment;
+
+public class CanvasFragment extends Fragment {
+
+}
--- app/src/main/java/edu/temple/colorchangingapp/MainActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/MainActivity.java
@@ -1,17 +1,14 @@
 package edu.temple.colorchangingapp;
 
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class PaletteActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_COLOR = "color";
     public static final String EXTRA_NAME = "name";
@@ -20,7 +17,7 @@ public class PaletteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_palette);
+        setContentView(R.layout.activity_main);
         setTitle(getString(R.string.palette_label));
 
         grid = findViewById(R.id.grid);
@@ -30,11 +27,20 @@ public class PaletteActivity extends AppCompatActivity {
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(PaletteActivity.this, CanvasActivity.class);
-                intent.putExtra("color", ((ColorDrawable)view.getBackground()).getColor());
-                intent.putExtra("name", ((TextView)view).getText());
-                PaletteActivity.this.startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, CanvasActivity.class);
+//                intent.putExtra("color", ((ColorDrawable)view.getBackground()).getColor());
+//                intent.putExtra("name", ((TextView)view).getText());
+//                MainActivity.this.startActivity(intent);
             }
         });
+
+//        Intent intent = getIntent();
+//
+//        layout = findViewById(R.id.layout);
+//        layout.setBackgroundColor(intent.getIntExtra(MainActivity.EXTRA_COLOR, Color.WHITE));
+//
+//        TextView label = findViewById(R.id.label);
+//        label.setText(intent.getStringExtra(MainActivity.EXTRA_NAME));
+
     }
 }
